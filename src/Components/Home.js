95bd6166b0d3f88a36597bea0fbf8cb00c98288a
vipul gupta <vipulgupta9594@gmail.com>
2020-06-25 19:42:45 -1200

    new commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -8,6 +8,9 @@ function Home() {
  <h1>I'm Vipul.</h1>
  <p>a <span className="web">web</span> developer.</p>
  </div>
+ <img  className="topCloud" src="images/cloud.png" alt="Top cloud" />
+ <img className="bottomCloud" src="images/cloud.png" alt="Bottom cloud" />
+ <img className="mountain" src="images/mountain.png" alt="Mountain" />
  </div>
 }
 
