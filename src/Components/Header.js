4cf797f7e0134e110e204b5a8e848da2340add37
vipul gupta <vipulgupta9594@gmail.com>
2020-06-27 20:45:03 -1200

    toggler commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,30 @@
 import React from "react";
 
 function Header() {
+    function handleClick() {
+        return  <div className="nav-menu">
+    <div className="nav-item">
+    <a href="https://www.facebook.com/vipdwayne"><i class="fa fa-facebook-square icon" aria-hidden="true"></i></a>
+    </div>
+    <div className="nav-item">
+    <a href="https://www.instagram.com/vipulgupta980/"><i class="fa fa-instagram icon" aria-hidden="true"></i></a>
+    </div>
+    <div className="nav-item">
+    <a href="https://www.linkedin.com/in/vipul-gupta-34b864168/"><i class="fa fa-linkedin-square icon" aria-hidden="true"></i></a>
+    </div>
+    <div className="nav-item">
+    <a href="https://twitter.com/Bringit98"><i class="fa fa-twitter-square icon" aria-hidden="true"></i></a>
+    </div>
+    <div className="nav-item">
+    <a href="https://github.com/vipul980"><i class="fa fa-github-square icon" aria-hidden="true"></i></a>
+    </div>
+     </div>
+    }
     return <nav className="navbar">
     <a className="nav-brand">Vipul Gupta</a>
+    <div  className="toggler-Hamburger">
+     <a className="hamIcon" onClick={handleClick}><i class="fa fa-bars" aria-hidden="true"></i></a>  
+    </div>
     <div className="nav-menu">
     <div className="nav-item">
     <a href="https://www.facebook.com/vipdwayne"><i class="fa fa-facebook-square icon" aria-hidden="true"></i></a>
