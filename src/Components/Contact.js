1a523e286b2f3f547076360035447fa37512c7fe
vipul gupta <vipulgupta9594@gmail.com>
2020-07-05 14:33:43 -1200

    form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,12 +1,25 @@
 import React from "react";
 
 function Contact() {
-    return <form>
+
+    return(
+     
+    <form>
+    <div className="holder">
     <input type="text" placeholder="Your Name" />
+    </div>
+    <div className="holder">
     <input type="text" placeholder="Email Address" />
+    </div>
+    <div className="holder">
     <input type="text" placeholder="Message" />
-    <input type="submit" placeholder="Submit" />
+    </div>
+    <div className="holder">
+    <a href="mailto:vipulgupta9594@gmail.com">Email me</a>
+    </div>
     </form>
+    );
+    
 }
 
 export default Contact;
