c1c84ac7f8a68bd85f2f0eb2656d222578e09a5e
vipul gupta <vipulgupta9594@gmail.com>
2020-07-09 20:13:41 -1200

    Resume update

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,7 +1,10 @@
 import React from "react";
 
+
+
 function Contact() {
-     const [name, setName] = React.useState("");
+
+
     
      function handleChange(e) {
         setName(e.target.value)
@@ -12,8 +15,10 @@ function Contact() {
         const Name = name;
         var char1 = Name.slice(0,1).toUpperCase();
         var remainingChar = Name.slice(1, Name.length).toLowerCase();
-
+         
          alert("Thankyou for the response" +" "+char1 + remainingChar+"!");
+         
+
          e.preventDefault();
          
      }
@@ -22,16 +27,16 @@ function Contact() {
      
     <form onSubmit={handleClick}>
     <div className="holder">
-    <input type="text" onChange={handleChange} placeholder="First Name" value={name}/>
+    <input type="text" name="fname" onChange={handleChange} placeholder="First Name" value={name}/>
     </div>
     <div className="holder">
-    <input type="text"  placeholder="Last Name"/>
+    <input type="text"  name="lname" placeholder="Last Name"/>
     </div>
     <div className="holder">
     <input type="email" name="Email" placeholder="Email Address"/>
     </div>
     <div className="holder">
-    <textArea placeholder="Type your message here" />
+    <textArea name="mssg" placeholder="Type your message here" />
     </div>
     <div className="holder">
     <input type="submit" placeholder="Submit" />
