09b9166ff51f7b34e3298481dd5e37824619d780
vipul gupta <vipulgupta9594@gmail.com>
2020-08-11 15:03:43 -1200

    Projects added

--- src/Components/About.js
+++ src/Components/About.js
@@ -33,10 +33,24 @@ function About() {
      </div>
      </div>
 
-    
-     
+    <div className="projects">
+    <h1>My Projects</h1>
+    <div className="projectRow">
+    <img className="secretImg" src="images/Secret.png" alt="Secret-web-app" />
+    <h3>Secrets</h3>
+    <p>Developed an Back-end web application with full authentication & security using Node js and Google,Facebook OAuth, Any user can register and login him or her self and can submit their secrets anonymously and all the registered users can see each others secret post without knowing whose secret belongs to whom.</p>
+    <a href="https://dark-secret2020.herokuapp.com/"><button className="secretButton">Have a look!</button></a>
+    </div>
 
+     <div className="projectRow">
+     <img className="drumkitImg" src="images/Drumkit.png" alt="Drumkit-web-pp" />
+     <h3>Drumkit</h3>
+     <p>Designed and Developed an drumkit web application with sound system using React js and UIFX module, It's basically an Drumkit which produces sound whenever the corresponding drum gets clicked or key press</p>
+     <a href="https://drumkit1998.netlify.app/"><button className="secretButton">Have a look!</button></a>
+     </div>    
      
+     </div>
+
      
      </div>
      
